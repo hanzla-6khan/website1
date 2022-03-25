963e5245f0f243a0b42f28b088880b52d7443373
hanzla-6khan <102364483+hanzla-6khan@users.noreply.github.com>
2022-03-25 19:32:17 +0500

    Add files via upload

---
+++ w2.js
@@ -0,0 +1,25 @@
+// (function(){
+
+//   const button = document.querySelectorAll('.btn')
+//   const storeImages = querySelectorAll('.store-item')
+
+
+//     //  targent our buttons 
+//     button.forEach(bu)
+// })();
+document.getElementsByClassName("btn") [1] .addEventListener("click",function(){
+  document.getElementsByTagName("img")[0].src="/pic/g2.jpg";  
+  document.getElementsByTagName("img")[1].src="/pic/g2.jpg";  
+  document.getElementsByTagName("img")[2].src="";  
+  document.getElementsByTagName("img")[3].src="";  
+  document.getElementsByTagName("img")[4].src=""; 
+  document.getElementsByTagName("img")[4].src=""; 
+});
+document.getElementsByClassName("btn") [0] .addEventListener("click",function(){
+  document.getElementsByTagName("img")[0].src="/pic/g3.jpg";  
+  document.getElementsByTagName("img")[1].src="/pic/g2.jpg";  
+  document.getElementsByTagName("img")[2].src="/pic/g3.jpg";  
+  document.getElementsByTagName("img")[3].src="/pic/g1.jpg";  
+  document.getElementsByTagName("img")[4].src="/pic/g4.jpg"; 
+  document.getElementsByTagName("img")[4].src="/pic/g6.jpg"; 
+});
